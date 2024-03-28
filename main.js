7287d8f47cb3b2ed70d82950ede04acdeb1f1419
lroux07 <lroux07@edenschool.fr>
2024-03-28 17:20:40 +0100

    Verify affected squares

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
 let squares = document.querySelectorAll('.squares');
+let currentPlayers = 1;
+
+const grid = [
+  [0, 0, 0],
+  [0, 0, 0],
+  [0, 0, 0]
+]
 
 for(let i = 0; i < squares.length; i++) {
   squares[i].addEventListener('click', () => {
-    squares[i].textContent = "X";
+    if(grid[i % 3][Math.floor(i / 3)] !== 0) {
+      return;
+    }
+    grid[i % 3][Math.floor(i / 3)] = currentPlayers;
+    
+    if(currentPlayers === 1) {
+      squares[i].textContent = "X";
+      currentPlayers = 2;
+    } else if(currentPlayers === 2) {
+      squares[i].textContent = "O";
+      currentPlayers = 1;
+    }
   });
 }
